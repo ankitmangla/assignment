28b619655cc7125283f45bca62ee4af977349ac8
Your Name <you@example.com>
2018-06-22 12:10:35 +0530

    ajbvd

---
+++ djh.py
@@ -0,0 +1,32 @@
+# Q1
+print("hello")
+
+# Q2
+string1 = "acad"
+string2 = "view"
+string3 = string1 + string2
+print(string3)
+
+# Q3
+x = 2
+y = 5
+z = 10
+print(x)
+print(y)
+print(z)
+
+# Q4
+print("Let's get started")
+
+# Q5
+s = "Acadview"
+course = "Python"
+fees = "5000"
+print(s)
+print(course)
+print(fees)
+
+# Q6
+name = "Tony Stark"
+salary = 1000000
+print("%s" "%d"%(name , salary)) 
